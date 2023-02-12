861ecddb4e4a55a34ba9235a858319eb80a2ebd3
xiaozhuang <2495580149@qq.com>
2023-02-12 13:51:32 +0800

    hot-fix commit2

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("我是小庄2");
         System.out.println("我是小庄3");
         System.out.println("我是小庄4");
+        System.out.println("hot-fix分支在此次修改");
     }
 }
