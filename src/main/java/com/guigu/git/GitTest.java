e893477753d0765e6280d5f7ea639129afc06fde
xiaozhuang <2495580149@qq.com>
2023-02-12 11:56:15 +0800

    third commit

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("我是小庄");
         System.out.println("我是小庄2");
+        System.out.println("我是小庄3");
     }
 }
